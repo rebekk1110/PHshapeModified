fb37f6054dcb4157c5ecf596f10c64b0f72b2c6f
Rebekka Øvensen Aanderaa <125483437+rebekk1110@users.noreply.github.com>
2024-10-18 13:25:47 +0200

    l

--- main_codes_gudhi/mdl1_bolPH_gu.py
+++ main_codes_gudhi/mdl1_bolPH_gu.py
@@ -5,17 +5,25 @@
 ------------------------------------------------------------------------------------------------------------------------
 @Description    : as below
 get basic building outlines by PH (gudhi version)
+get from segmentatied image1
+
+
 """
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import json
 import os
 import numpy as np
 import pandas as pd
+import numpy as np
 
 from tqdm import tqdm
-
 from utils.mdl_geo import poly2Geojson
-from utils.mdl_io import load_cloud
+
+#from utils.mdl_io import load_cloud
 from modules.get_basic_ol_v2_gu import get_autooptim_bf_radius_GU, get_build_bf
 from utils.mdl_procs import pre_downsampling
 
@@ -25,7 +33,29 @@ from utils.mdl_procs import pre_downsampling
 # pd.set_option('max_colwidth',100) #set the visual length of values to 100. default=50
 # pd.set_option('display.width',300) #set the overall display width
 
+testBig='test_images/trd_1_crop_188_3-kopi.tif'
+testSmall='test_images/trd_1_crop_224_4-kopi.tif'
+testImage='test_images/trd_1_crop_130_1-kopi.tif'
 
+def load_tif(file_path):
+    with rasterio.open(file_path) as src:
+        # Read all bands
+        data = src.read()
+        
+        # Get coordinates
+        height = src.height
+        width = src.width
+        cols, rows = np.meshgrid(np.arange(width), np.arange(height))
+        xs, ys = rasterio.transform.xy(src.transform, rows, cols)
+        
+        # Combine coordinates and elevation into a single array
+        coords = np.vstack((np.array(xs).ravel(),
+                            np.array(ys).ravel(),
+                            data.ravel())).transpose()
+        
+        return coords
+
+    
 
 def main_basicOL(data_root_folder:str,
                  data_type:str,
@@ -39,8 +69,10 @@ def main_basicOL(data_root_folder:str,
                  savename_bfr:str="",
                  is_unrefresh_save:bool=False,
                  is_Debug:bool=False):
-
+    
     num_bld = len(bld_list)
+
+    print(num_bld)
 
     # savename_bfr = os.path.join(os.path.dirname(out_folder), f"all_bfrs_optim.csv")
     if is_use_saved_bfr:
@@ -52,6 +84,7 @@ def main_basicOL(data_root_folder:str,
         if is_Debug:
             print("the file saving bfr_optim DOESN'T existed, and will be calculted...")
 
+
     with tqdm(range(num_bld)) as pbar_blds:
         for bldi in pbar_blds:
             pbar_blds.set_description(f"[basicOL] :: bld_{bldi}-{bld_list[bldi]}")
@@ -61,23 +94,42 @@ def main_basicOL(data_root_folder:str,
             savename = os.path.join(out_folder, f"{bldi_name}.json")
             if (is_unrefresh_save) and (os.path.exists(savename)):
                 continue
+            
+            # Read TIF data
+            bldi_path = os.path.join(data_root_folder, f"{bldi_name}{data_type}")
+            bldi_3d = load_tif(bldi_path)
+            bldi_2d = bldi_3d[:, :2]  # Use only x and y coordinates
+            if is_Debug:
+                print(f"[bldi_name={bldi_name}] :: the shape of input data is: ", bldi_2d.shape)
 
+            
+            """
             # read point cloud data
             bldi_path = os.path.join(data_root_folder, f"{bldi_name}{data_type}")
             bldi_2d = load_cloud(bldi_path)[:,:2]
             if is_Debug:
                 print(f"[bldi_name={bldi_name}] :: the shape of input cloud is: ", bldi_2d.shape)
+            """
 
             ##############
             # pre-processing
             # reduce the number of points by voxel_sampling to speed up PH(0d)
             ##############
+
+            if bldi_2d.shape[0] > pre_cloud_num:
+                bldi_2d, used_vs = pre_downsampling(bldi_2d, target_num=pre_cloud_num, start_voxel_size=0.5,
+                                                    isDebug=is_Debug)
+                if is_Debug:
+                    print(f"[bldi_name={bldi_name}] :: the shape of down_sampled input data is: ", bldi_2d.shape)
+
+            """
+            #Point cloud pre-prosses
             if bldi_2d.shape[0] > pre_cloud_num:
                 bldi_2d, used_vs = pre_downsampling(bldi_2d, target_num=pre_cloud_num, start_voxel_size=0.5,
                                                     isDebug=is_Debug)
                 if is_Debug:
                     print(f"[bldi_name={bldi_name}] :: the shape of down_sampled input cloud is: ", bldi_2d.shape)
-
+             """
             ##############
             # get auto-optimized buffer radius
             ##############
@@ -115,3 +167,7 @@ def main_basicOL(data_root_folder:str,
     if is_use_saved_bfr==False and len(all_bfr_optim)!=0:
         all_bfr_optim_bf = pd.DataFrame(all_bfr_optim, columns=["bid", "bfr_optim"])
         all_bfr_optim_bf.to_csv(savename_bfr, index=False)
+
+
+
+
